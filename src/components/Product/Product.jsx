5c67a9983cf004ffe106d03d576c5ebc10ebea24
jacobadam <jacobanevitt@gmail.com>
2021-04-15 14:50:12 -0700

    add component for single Product

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Card, CardMedia, CardContent, CardActions, Typography, IconButton} from '@material-ui/core';
+import {AddShoppingCart} from '@material-ui/icons';
+
+const Product = () => {
+  return (
+    <Card className={classes.root}>
+      <CardMedia classname={classes.media} image='' title={product.name} />
+    </Card>
+  )
+}
+
+export default Product
