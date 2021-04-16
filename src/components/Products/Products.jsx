dbba7de883a9ec09a5c58e472946b9da62e9e26a
jacobadam <jacobanevitt@gmail.com>
2021-04-15 17:12:05 -0700

    updated Products file to jsx

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Grid from "@material-ui/core";
+
+const products = [
+  { id: 1, name: "shoes", description: "running shoes", price: '$5' },
+  { id: 2, name: "jumper", description: "jumper with logo", price: '$10' },
+];
+
+const Products = () => {
+  <main>
+    <Grid container justify="center" spacing={4}>
+      {products.map((product) => (
+        <Grid item key={product.id} xs={12} sm={6} m={4} l={3}>
+          <Product products={product} />
+        </Grid>
+      ))}
+    </Grid>
+  </main>;
+};
+
+export default Products;
