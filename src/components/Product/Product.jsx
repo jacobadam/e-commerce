8067157676f78d6e16a4cb90990706a7028a2193
jacobadam <jacobanevitt@gmail.com>
2021-04-15 17:17:17 -0700

    add styles page

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -15,7 +15,15 @@ const Product = ({product}) => {
             {product.price}
           </Typography>
         </div>
+        <Typography varient="h2" color="textSecondary">
+            {product.description}
+          </Typography>
       </CardContent>
+      <CardActions disableSpacing className={classes.CardActions}>
+        <IconButton aria-label="Add to Cart">
+          <AddShoppingCart/>
+        </IconButton>
+      </CardActions>
     </Card>
   )
 }
