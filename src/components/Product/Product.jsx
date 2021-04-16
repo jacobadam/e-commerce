82c304a3dc20a4e56b2bfbb87d7bb480d2a8e7f0
jacobadam <jacobanevitt@gmail.com>
2021-04-15 17:11:38 -0700

    added more content to Product component and passed in props

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import {Card, CardMedia, CardContent, CardActions, Typography, IconButton} from '@material-ui/core';
 import {AddShoppingCart} from '@material-ui/icons';
 
-const Product = () => {
+const Product = ({product}) => {
   return (
     <Card className={classes.root}>
       <CardMedia classname={classes.media} image='' title={product.name} />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography varient="h5" gutterBottom>
+            {product.name}
+          </Typography>
+          <Typography varient="h5" gutterBottom>
+            {product.price}
+          </Typography>
+        </div>
+      </CardContent>
     </Card>
   )
 }
